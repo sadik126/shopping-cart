879c997cf2e293dda16ebeb0c5bc4b1d0bd9c3e8
sadik126 <sadikhimel04@gmail.com>
2022-02-13 20:55:02 +0600

    m

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 document.getElementById('caseplus').addEventListener('click',function(){
     let input = document.getElementById('input');
     let increase = input.value;
-    total = parseInt(increase)+1;
+    let total = parseInt(increase)+1;
     input.value = total;
     
     totalprice = input.value * 59;
     let price = document.getElementById('casetotal');
     price.innerText = totalprice;
+    totalp();
 })
 
 
@@ -14,13 +15,15 @@ document.getElementById('caseminus').addEventListener('click',function(){
     let input = document.getElementById('input');
     let increase = input.value;
     if(increase > 0){
-        total = parseInt(increase)-1;
+       let total = parseInt(increase)-1;
+       input.value = total;
     }
     
-    input.value = total;
+    
     totalprice = input.value * 59;
     let price = document.getElementById('casetotal');
     price.innerText = totalprice;
+    totalp();
    
     
 })
@@ -30,13 +33,15 @@ document.getElementById('phoneminus').addEventListener('click',function(){
     let input = document.getElementById('input1');
     let increase = input.value;
     if(increase > 0){
-        total = parseInt(increase)-1;
+       let total = parseInt(increase)-1;
+       input.value = total;
     }
     
-    input.value = total;
+    
     totalprice = input.value * 1219;
     let price = document.getElementById('phonetotal');
     price.innerText = totalprice;
+    totalp();
    
     
 })
@@ -51,4 +56,41 @@ document.getElementById('phoneplus').addEventListener('click',function(){
     totalprice = input.value * 1219;
     let price = document.getElementById('phonetotal');
     price.innerText = totalprice;
+    totalp();
+   
 })
+
+
+function phonecalculate(){
+    let input = document.getElementById('input1');
+    totalprice = parseInt(input.value) ;
+    return totalprice;
+}
+
+function casecalculate(){
+    let input = document.getElementById('input');
+    totalprice = parseInt(input.value) ;
+    return totalprice;
+}
+
+
+
+function totalp(){
+   let phoneprice = phonecalculate()*1219;
+   let caseprice = casecalculate()*59;
+   let total = phoneprice + caseprice;
+   let tax = total /10;
+   let totalprice = tax + total;
+   document.getElementById('sub-total').innerText = total;
+   document.getElementById('tax').innerText = tax;
+   document.getElementById('total').innerText = totalprice;
+   
+}
+
+
+
+
+
+// let phonetotal = document.getElementById('phonetotal') 
+// let total1 = phonetotal.innerText;
+// console.log(total1);
