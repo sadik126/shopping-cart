10e6aa1627eca28ace16331277d943fdfcf1e202
sadik126 <sadikhimel04@gmail.com>
2022-02-13 18:54:50 +0600

    m

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ document.getElementById('caseplus').addEventListener('click',function(){
     total = parseInt(increase)+1;
     input.value = total;
 })
+
+
+document.getElementById('caseminus').addEventListener('click',function(){
+    let input = document.getElementById('input');
+    let increase = input.value;
+    if(increase > 0){
+        total = parseInt(increase)-1;
+    }
+    
+    input.value = total;
+})
